d31a71a9d4972949ccd84b4cd8b81e3b17ec6010
Martin Lerude <77109266+Nitram-Edurel@users.noreply.github.com>
2021-10-17 18:01:59 +0200

    Add files via upload

---
+++ installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '969007041b2c847b695ce37a54671883487e46bb',
+        'name' => 'wilder/composer_quest',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wilder/composer_quest' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '969007041b2c847b695ce37a54671883487e46bb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
